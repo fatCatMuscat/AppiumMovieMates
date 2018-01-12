3eace3d7f9176e37f2ed1b8d6923a3ef4f1f737e
arsenal <strigunovqa@gmail.com>
2018-01-11 18:41:42 -0800

    aded boundary test 51char username

--- src/main/java/AcceptanceTests/UserProfile.java
+++ src/main/java/AcceptanceTests/UserProfile.java
@@ -4,11 +4,11 @@
         import ScreenFactories.MoviesScreenFactory;
         import ScreenObjects.LoginScreen;
         import ScreenObjects.MoviesScreen;
-import Utils.BaseTest;
-import org.openqa.selenium.By;
-import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+        import Utils.BaseTest;
+        import org.openqa.selenium.By;
+        import org.testng.Assert;
+        import org.testng.annotations.AfterMethod;
+        import org.testng.annotations.BeforeMethod;
         import org.testng.annotations.DataProvider;
         import org.testng.annotations.Test;
 
@@ -17,8 +17,6 @@ public class UserProfile extends BaseTest {
     public MoviesScreenFactory moviesScreenFactory = new MoviesScreenFactory();
     @BeforeMethod
     private void successfulGoogleLoginWithValidCredential() {
-        System.out.println("login");
-
         LoginScreen loginScreen = new LoginScreen();
 
         loginScreen.clickLoginGoogleBtn();
@@ -43,20 +41,44 @@ public class UserProfile extends BaseTest {
     public Object[][] getValidNames() {
         return new Object[][] {
                 {"Hruysha", "Filya"},
-                {"Karkusha", "Stepashka"},
+                {"Karkusha Polikarpovna", "Polikarp Himself"},
+        };
+    }
+
+    @DataProvider(name = "longNamesProvider")
+    public Object[][] getLongNames() {
+        return new Object[][] {
+                {"Randolph Sherman Thomas Uncas Victor William Xerxes"},
+                {"YancyWolfeschlegelsteinhausenbergerdorffSeniorwholivedinPhiladelphia"}
         };
     }
 
     @Test(dataProvider = "validNamesProvider")
-    public void changeNamePOM(String[] validNames) {
+    public void changeValidName(String[] validNames) {
         MoviesScreen moviesScreen = new MoviesScreen();
         String newName = validNames[0];
-        MoviesScreen secondMoviesScreen = moviesScreen.clickProfileButton().clickEditName().removeName()
-                .enterName(newName).clickOkButton().clickMoviesButton();
+        MoviesScreen secondMoviesScreen = moviesScreen.clickProfileButton()
+                .clickEditName().removeName().enterName(newName)
+                .clickOkButton().clickMoviesButton();
         String currentName = secondMoviesScreen.clickProfileButton().getCurrentName();
         System.out.println(currentName);
         Assert.assertEquals(currentName, newName);
     }
+
+    @Test(dataProvider = "longNamesProvider")
+    public void nameMorethan50Char(String[] longNames) {
+        MoviesScreen moviesScreen = new MoviesScreen();
+        String newName = longNames[0];
+        MoviesScreen secondMoviesScreen = moviesScreen.clickProfileButton()
+                .clickEditName().removeName().enterName(newName)
+                .clickOkButton().clickMoviesButton();
+        String currentName = secondMoviesScreen.clickProfileButton().getCurrentName();
+        System.out.println(currentName);
+        Assert.assertNotEquals(currentName, newName);
+    }
+
+
+
 
 
 
